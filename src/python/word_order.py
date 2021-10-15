acee2f9862ad5eba3b237fbd2cefddf601a5b0fb
BALAKRISHNAN RADHAKRISHNAN <bala@Thendral.local>
2021-10-14 23:19:54 -0400

    soltuon for https://www.hackerrank.com/challenges/word-order/problem

--- src/python/word_order.py
+++ src/python/word_order.py
@@ -1,10 +1,11 @@
+from typing import OrderedDict
+
 # solution for https://www.hackerrank.com/challenges/word-order/problem
 
 n = int(input())
 words = [input() for _ in range(n)]
-word_counts = {}
+word_counts = OrderedDict()
 for word in words:
     word_counts[word] = word_counts[word] + 1 if word in word_counts else 1
 print(len(word_counts))
-for word in word_counts:
-    print(word_counts[word], end=" ")
+print(*word_counts.values())
